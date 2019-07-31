ddf5ab3cad6703db9ee1d7f79cb96752cf419cc2
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-07-31 11:54:10 +0300

    loop zaloop

--- time1.sh
+++ time1.sh
@@ -2,18 +2,8 @@
 echo "Start"
 while (true) 
 do
- ./75.sh;
+ ./zaloop.sh;
  sleep 75; 
-  ./50.sh;
- sleep 98; 
-   ./80.sh;
- sleep 85; 
-   ./65.sh;
- sleep 78; 
-   ./90.sh;
- sleep 69; 
-  ./40.sh;
- sleep 99; 
 done;
 
 
